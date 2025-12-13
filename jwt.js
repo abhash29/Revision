e88f5b2b408ad596197c51d871fa961197a8e164
ABHASH KUMAR DAS <108912904+abhash29@users.noreply.github.com>
2025-12-13 21:32:32 +0530

    Integrate MongoDB and implement user signup
    Added MongoDB connection and user signup functionality.

--- jwt.js
+++ jwt.js
@@ -3,6 +3,10 @@ const app = express();
 app.use(express.json());
 const jwt = require('jsonwebtoken');
 const jwtPassword = '123456';
+const mongoose = require('mongoose');
+const { email } = require('zod');
+
+mongoose.connect("mongodb+srv://abhash29:Abhash%406685@cluster0.07ms2qn.mongodb.net/users");
 
 const AllUsers = [
     {
@@ -20,6 +24,12 @@ const AllUsers = [
         name: "Priya singh"
     },
 ]
+
+//Mongodb
+const User = mongoose.model("Users", {username: String, password: String, name: String});
+//const user = new User({username: "abhash29@gamil", password: "124", name: "abhash"});
+//user.save();
+
 
 function userExists(username, password){
     for(let i=0; i<AllUsers.length; i++){
@@ -51,6 +61,26 @@ app.get('/users', (req, res) => {
     }
     catch(err){
         return res.status(403).json({msg: "Invalid token"});
+    }
+})
+
+app.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const existingUser = await User.findOne({username: username});
+    if(existingUser){
+        return res.status(411).json({msg: "User already exits"});
+    }
+    else{
+        const user = new User({
+            username: username,
+            password: password,
+            name: name,
+        });
+        await user.save();
+        res.status(200).json({msg: "User created successfully"});
     }
 })
 
